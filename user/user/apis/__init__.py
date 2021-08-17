c8629b189915af289f792f17a2b4b5643b290e1d
BACK Yonah <yonah.back@telecomnancy.eu>
2021-08-17 03:15:28 +0200

    Create app switched back in app.py

--- user/user/apis/__init__.py
+++ user/user/apis/__init__.py
@@ -5,44 +5,11 @@
 Core initialisation of the API
 """
 
-import os
-from flask import Flask, g, Blueprint
-from pony.flask import Pony
+from flask import Blueprint
 from flask_socketio import SocketIO
 from flask_restx import Api
-from tools.db import db, initdb
-from werkzeug.middleware.proxy_fix import ProxyFix
 
 socketio = SocketIO(cors_allowed_origins="*")
-def create_app():
-    """Return the flask app for the example microservice"""
-    app = Flask(__name__)
-    app.wsgi_app = ProxyFix(app.wsgi_app)
-
-    app.config['RESTPLUS_VALIDATE'] = True
-    
-    app.register_blueprint(apis)
-
-    app.config.update(dict(
-        DEBUG = False,
-        SECRET_KEY = 'testo',
-        PONY = {
-            'provider': 'postgres',
-            'host': 'database',
-            'database': 'user',
-            'port': '5432',
-            'password': os.environ.get('DB_PASSWORD'),
-            'user':'service_user'
-        }
-    ))
-    initdb(app)
-    Pony(app)
-    socketio.init_app(app)
-    return app
-
-
-
-
 
 
 from apis.role import api as nsrole
--- user/user/app.py
+++ user/user/app.py
@@ -4,9 +4,43 @@
 """
 Main functions for the example microservice flask app
 """
+import os
+from flask import Flask, g
+from pony.flask import Pony
+from flask_socketio import SocketIO
+from flask_restx import Api
+from tools.db import db, initdb
+from werkzeug.middleware.proxy_fix import ProxyFix
+from apis import socketio, apis
 
-from apis import create_app, socketio
-app = create_app()
+def create_app():
+    """Return the flask app for the example microservice"""
+    app = Flask(__name__)
+    app.wsgi_app = ProxyFix(app.wsgi_app)
+
+    app.config['RESTPLUS_VALIDATE'] = True
+    
+    app.register_blueprint(apis)
+
+    app.config.update(dict(
+        DEBUG = False,
+        SECRET_KEY = 'testo',
+        PONY = {
+            'provider': 'postgres',
+            'host': 'database',
+            'database': 'user',
+            'port': '5432',
+            'password': os.environ.get('DB_PASSWORD'),
+            'user':'service_user'
+        }
+    ))
+    initdb(app)
+    Pony(app)
+    socketio.init_app(app)
+    return app
+
+
+
 if __name__ == '__main__':
-
+    app = create_app()
     socketio.run(app)
